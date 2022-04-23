1704ad8c6405fa14eb2c657413c48b1607bb07c5
michaelherbut <96901788+michaelherbut@users.noreply.github.com>
2022-04-23 03:34:21 +0200

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#tested on Debian11
+
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt -y install wget docker-ce docker-ce-cli containerd.io
+sudo apt install -y curl
+curl -L https://github.com/docker/compose/releases/download/2.4.1/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+chmod +x ~/docker-compose
+sudo mv ~/docker-compose /usr/local/bin/docker-compose
